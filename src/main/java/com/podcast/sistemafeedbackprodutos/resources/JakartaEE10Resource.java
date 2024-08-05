eceacb96f25221ec9b5cdeab9fb5a361a83c69aa
Jean Kanesse <jkanesse@gmail.com>
2024-08-05 01:15:05 -0400

    Projeto

---
+++ src/main/java/com/podcast/sistemafeedbackprodutos/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.podcast.sistemafeedbackprodutos.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
